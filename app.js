3aa70aedc95f09733c9aaa60a260fa55d83eb849
onetaek <dnjsxorchlrh@naver.com>
2022-01-27 12:47:34 +0900

    2022-01-27(챌린지 완성작품!+ClearAll버튼+선굵기 값 보여주기

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");//canvas의 context를 2D로 설정
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+const rangeValue = document.querySelector(".controls__range :nth-child(2)");
+const clearAllBtn = document.getElementById("jsClearAll");
 
 //픽셀을 다루는 윈도우가 얼마나 큰지 크기를 지정해주어야한다.
 canvas.width = 700;
 canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.lineCap = "round";
+ctx.fillStyle = "white";
+//초기색을 지정하지않으면 배경색을 건드리지않고 저장했을 때 투명으로 저장됨
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;//단위 : px 
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -16,6 +34,9 @@ function stopPainting(){
 
 function startPainting(){
     painting = true;
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
 }
 
 function onMouseMove(e){
@@ -33,14 +54,77 @@ function onMouseMove(e){
     }
 }
 
-function onMouseDown(e){
-    painting = true;
+function handleColorClick(e){
+    const color = e.target.style.backgroundColor;
+    ctx.strokeStyle = color;//클릭하면 펜의 색을 클릭한 색으로 바꾼다.
+    ctx.fillStyle = color;//클릭하면 채우기 색을 클릭한 색으로 바꾼다.
 }
 
+function handleRangeChange(e){
+    const size = e.target.value;
+    ctx.lineWidth = size;
+    rangeValue.value = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick(){
+    
+}
+
+function handleCM(e){//contextmenu(캔버스위에 마우스 우클릭하면 생기는 메뉴)
+    //contextmenu가 생기는 것을 없에는 함수
+    e.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
+function handleClearAllClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+}
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+//colors를 console을 했는데 배열의 형태가 아니여서 배열로 바꿔주었다. 
+//array.from(object)메소드는 object로 부터 array를 만든다.
+Array.from(colors).forEach(color => 
+    color.addEventListener("click",handleColorClick)
+    );
+
+if(range){
+    range.addEventListener("input",handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
+}
+
+if(clearAllBtn){
+    clearAllBtn.addEventListener("click",handleClearAllClick);
 }
